1fd89594f60bdef88fa72d3caa72ee79489bd8d4
Vincenzo Pacella <shaduz@shaduzlabs.com>
2018-02-13 12:09:54 +0100

    try to find asio standalone locally first

--- cmake_include/AsioStandaloneConfig.cmake
+++ cmake_include/AsioStandaloneConfig.cmake
@@ -17,11 +17,6 @@ else()
   )
 endif()
 
-set_property(TARGET AsioStandalone::AsioStandalone APPEND PROPERTY
-  INTERFACE_INCLUDE_DIRECTORIES
-  ${CMAKE_CURRENT_LIST_DIR}/../modules/asio-standalone/asio/include
-)
-
 if (WIN32)
   set_property(TARGET AsioStandalone::AsioStandalone APPEND PROPERTY
     INTERFACE_COMPILE_DEFINITIONS
